724a7a27e5fec4e4d3730b6031d215862c58fff3
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2025-01-14 23:58:23 +0000

    feat: add sidebar for testing

--- app.vue
+++ app.vue
@@ -3,15 +3,57 @@ const version = 2 + 1
 </script>
 
 <template>
-  <div class="hello">
-    Hello Nuxt {{ version }}!
+  <div class="dashboard">
+    <aside class="sidebar">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Profile</a></li>
+        <li><a href="#">Settings</a></li>
+        <li><a href="#">Logout</a></li>
+      </ul>
+    </aside>
+    <main class="content">
+      <div class="hello">
+        Hello Nuxt {{ version }}!
+      </div>
+    </main>
   </div>
 </template>
 
 <style scoped>
+.dashboard {
+  display: flex;
+  height: 100vh;
+}
+
+.sidebar {
+  width: 250px;
+  background-color: #2c3e50;
+  color: white;
+  padding: 1rem;
+}
+
+.sidebar ul {
+  list-style: none;
+  padding: 0;
+}
+
+.sidebar ul li {
+  margin: 1rem 0;
+}
+
+.sidebar ul li a {
+  color: white;
+  text-decoration: none;
+}
+
+.content {
+  flex: 1;
+  padding: 2rem;
+}
+
 .hello {
   font-family: Arial, Helvetica, sans-serif;
   font-size: 3rem;
-  padding: 2rem;
 }
 </style>
