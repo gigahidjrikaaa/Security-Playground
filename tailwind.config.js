d4d08ee28cf44e79bb71da01f7faefe80e603286
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2025-01-15 00:18:08 +0000

    feat: migrate Tailwind CSS imports to main.css and update content paths in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{vue,js}',
+    './components/**/*.{vue,js,ts}',
     './layouts/**/*.{vue,js}',
     './pages/**/*.{vue,js}',
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
+    './app.vue',
+    './error.vue',
   ],
   theme: {
     extend: {},
